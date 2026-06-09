13e351dccfd69e1c517f4bb7e15dd6d80343b318
Or Shkefati <or.shkefati@gmail.com>
2026-06-09 15:51:57 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // כל פנייה באפליקציה ל- /api-football תופנה אוטומטית לשרת הנתונים
-      '/api-football': {
-        target: 'https://api.football-data.org',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-football/, '')
-      }
-    }
-  }
 })
